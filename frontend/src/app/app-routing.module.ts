50943ea65833f061697bff5295a03059cbac3fc8
Chris Apaza <chrisalanapazaa@gmail.com>
2023-09-19 21:51:00 -0400

    Established initial Angular project file structure

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProfessorScheduleComponent } from './pages/professor-schedule/professor-schedule.component';
+import { StudentScheduleComponent } from './pages/student-schedule/student-schedule.component';
+import { StudentListComponent } from './pages/student-list/student-list.component';
+import { StudentKardexComponent } from './pages/student-kardex/student-kardex.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'professor/schedule', component: ProfessorScheduleComponent },
+  { path: 'student/schedule', component: StudentScheduleComponent },
+  { path: 'student/list', component: StudentListComponent },
+  { path: 'student/kardex', component: StudentKardexComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
