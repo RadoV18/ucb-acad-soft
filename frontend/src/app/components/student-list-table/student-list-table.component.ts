d247c1d5708cf7d847867f2626e7d3cf9dd98d86
Chris Apaza <chrisalanapazaa@gmail.com>
2023-09-21 12:28:47 -0400

    TDS-15 added table to display student contact info, and a button to download the csv

--- frontend/src/app/components/student-list-table/student-list-table.component.ts
+++ frontend/src/app/components/student-list-table/student-list-table.component.ts
@@ -36,6 +36,17 @@ export class StudentListTableComponent implements OnInit {
       next: (response) => {
         // console.log(response.data);
         this.studentList = response.data;
+        console.log(response.data);
+        this.studentList = response.data;
+        this.studentList.sort((a, b) => {
+          if (a.lastName > b.lastName) {
+            return 1;
+          }
+          if (a.lastName < b.lastName) {
+            return -1;
+          }
+          return 0;
+        });
       },
       error: (error) => {
         console.log(error);
--- frontend/src/app/services/student-list.service.ts
+++ frontend/src/app/services/student-list.service.ts
@@ -4,22 +4,15 @@ import {Observable} from "rxjs";
 import {SemesterDto} from "../dto/semester.dto";
 import {ResponseDto} from "../dto/response.dto";
 import { SubjectDto } from '../dto/subject.dto';
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { SubjectDetailDto } from '../dto/subject-detail.dto';
 import {StudentInfoDto} from "../dto/student-info.dto";
 import {FileDto} from "../dto/file.dto";
-=======
->>>>>>> fd027f7 (TDS-15 added card for searching subjects by professor and semester)
-=======
-import { SubjectDetailDto } from '../dto/subject-detail.dto';
->>>>>>> 920818a (TDS-15 added card for the subject detail info)
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class StudentListService {
-<<<<<<< HEAD
   baseUrl = 'http://localhost:5260/api/v1/reports';
   constructor(private http: HttpClient) { }
 
@@ -42,23 +35,4 @@ export class StudentListService {
   public getStudentListCSVBySubjectIdAndSemesterId(subjectId: number, semesterId: number): Observable<ResponseDto<FileDto>> {
     return this.http.get<ResponseDto<FileDto>>(`${this.baseUrl}/students/csv/subjects/${subjectId}?semesterId=${semesterId}`);
   }
-=======
-  baseUrl = 'http://localhost:8080/api/v1/reports';
-  constructor(private http: HttpClient) { }
-
-  public getSemestersByProfessorId(professorId: number): Observable<ResponseDto<SemesterDto[]>> {
-    return this.http.get<ResponseDto<SemesterDto[]>>(`${this.baseUrl}/professors/${professorId}/semester`);
-  }
-
-  public getSubjectsByProfessorIdAndSemesterId(professorId: number, semesterId: number): Observable<ResponseDto<SubjectDto[]>> {
-    return this.http.get<ResponseDto<SubjectDto[]>>(`${this.baseUrl}/professors/${professorId}/subjects?semesterId=${semesterId}`);
-  }
-  
-<<<<<<< HEAD
->>>>>>> fd027f7 (TDS-15 added card for searching subjects by professor and semester)
-=======
-  public getSubjectDetailsBySubjectId(subjectId: number): Observable<ResponseDto<SubjectDetailDto>> {
-    return this.http.get<ResponseDto<SubjectDetailDto>>(`${this.baseUrl}/subjects/${subjectId}`);
-  }
->>>>>>> 920818a (TDS-15 added card for the subject detail info)
 }
