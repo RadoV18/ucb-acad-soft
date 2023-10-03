89ea2447c8023a66f79c385ee31f81c1f398a6cf
Sebastian Francisco Belmonte Cerveró <sebastian.belmonte01@gmail.com>
2023-10-03 17:05:37 -0400

    TDS-25 table made on request kardex

--- frontend/src/app/pages/student-kardex-request/student-kardex-request.component.ts
+++ frontend/src/app/pages/student-kardex-request/student-kardex-request.component.ts
@@ -1,10 +1,72 @@
 import { Component } from '@angular/core';
+import {RequestKardex} from "../../models/RequestKardex";
 
 @Component({
   selector: 'app-student-kardex-request',
   templateUrl: './student-kardex-request.component.html',
   styleUrls: ['./student-kardex-request.component.sass']
 })
+
 export class StudentKardexRequestComponent {
+
+
+
+  displayedColumns: string[] = [
+    'requestId',
+    'requestDate',
+    'requestStatus',
+    'requestDetail'
+  ];
+
+  dataSource : RequestKardex[] = [
+    {
+      id: 1,
+      date: "2021-05-01",
+      status: "Pendiente",
+      detail: {
+        reason: "Porque si",
+        image: "url"
+      }
+    },
+    {
+      id: 2,
+      date: "2021-05-01",
+      status: "Pendiente",
+      detail: {
+        reason: "Porque si",
+        image: "url"
+      }
+    },
+    {
+      id: 3,
+      date: "2021-05-01",
+      status: "Pendiente",
+      detail: {
+        reason: "Porque si",
+        image: "url"
+      }
+    },
+    {
+      id: 4,
+      date: "2021-05-01",
+      status: "Rechazado",
+      detail: {
+        reason: "Porque si",
+        image: "url"
+      }
+    },
+    {
+      id: 5,
+      date: "2021-05-01",
+      status: "Aceptado",
+      detail: {
+        reason: "Porque si",
+        image: "url"
+      }
+    },
+  ];
+
+
+
 
 }
