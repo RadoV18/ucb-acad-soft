6c5c2a51d13d6baace1b7d213b1f4d754ef6b650
Radomir Vladislavić <vladislavic.rl@gmail.com>
2023-10-11 16:19:27 -0400

    TDS-14 add web service to generate kardex

--- Backend/Controllers/KardexReportsController.cs
+++ Backend/Controllers/KardexReportsController.cs
@@ -11,6 +11,10 @@ public class KardexReportsController : ControllerBase
 {
 
     private readonly KardexService _kardexService = new KardexService();
+    private readonly Services.StudentAndProfessorService _studentAndProfessorService =
+        new Services.StudentAndProfessorService();
+    private readonly Services.PdfTurtleService _pdfTurtleService = new Services.PdfTurtleService();
+    private readonly Services.MinioService _minioService = new Services.MinioService();
 
     [HttpGet]
     public async Task<ActionResult<KardexSummaryDTO>> GetKardexReport()
@@ -22,6 +26,44 @@ public class KardexReportsController : ControllerBase
             var data = new KardexSummaryDTO(kardex.Data, summary.Data);
             var response = new ResponseDTO<KardexSummaryDTO>(data, null, true);
             return Ok(data);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+
+    [HttpGet("pdf")]
+    public async Task<ActionResult<string>> GetKardexReportPdf()
+    {
+        try
+        {   
+            var header = System.IO.File.ReadAllText("Utils/PdfTemplates/StudentKardex/header.html");
+            var footer = System.IO.File.ReadAllText("Utils/PdfTemplates/StudentKardex/footer.html");
+            var body = System.IO.File.ReadAllText("Utils/PdfTemplates/StudentKardex/index.html");
+
+            var kardex = await _kardexService.GetMyKardex();
+            var student = await _studentAndProfessorService.GetStudentById(1);
+            var model = new
+            {
+                student = student,
+                kardex = new {
+                    data = kardex.Data,
+                }
+            };
+
+            byte[] pdf = await _pdfTurtleService.getPdf(footer, header, body, model);
+
+            // upload pdf to minio
+            Guid guid = Guid.NewGuid();
+            string fileName = $"{guid}.pdf";
+            var newFileDto = await _minioService.UploadFile("pdf", fileName, pdf, "application/pdf");
+            return Ok(new ResponseDTO<string>
+            (
+                newFileDto.DownloadLink,
+                null,
+                true
+            ));
         }
         catch (Exception e)
         {
