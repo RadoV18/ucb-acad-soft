ac60f863993374166034626b302946fec145a5de
Sebastian Francisco Belmonte Cerveró <sebastian.belmonte01@gmail.com>
2023-10-12 16:32:24 -0400

    Adding continuous evaluation frontend

--- frontend/src/app/pages/continuous-evaluation/continuous-evaluation.component.ts
+++ frontend/src/app/pages/continuous-evaluation/continuous-evaluation.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { StudentContinuousEvaluation, Subject, SubjectContinuousGrades } from 'src/app/interfaces/interfaces';
 import { SubjectsService } from 'src/app/services/subjects.service';
+import {StudentListService} from "../../services/student-list.service";
+import {SubjectDto} from "../../dto/subject.dto";
+import {ContinuosEvaluationService} from "../../services/continous-evaluation/continuos-evaluation.service";
 
 @Component({
   selector: 'app-continuous-evaluation',
@@ -8,88 +11,38 @@ import { SubjectsService } from 'src/app/services/subjects.service';
   styleUrls: ['./continuous-evaluation.component.sass']
 })
 export class ContinuousEvaluationComponent {
+  professorId = 1;
+  semesterId = 4;
 
-  evaluations : StudentContinuousEvaluation[] = [ 
-    {
-      name : "Juan Perez",
-      continuousEvaluation : [
-        {
-          name : "Tarea 1",
-          grade : 9
-        },
-        {
-          name : "Tarea 2",
-          grade : 10
-        },
-        {
-          name : "Tarea 3",
-          grade : 5
-        },
-        {
-          name : "Tarea 4",
-          grade : 11
-        }],
-      finalGrade : 10 
-    },
-    {
-      name : "Juan Pereza",
-      continuousEvaluation : [
-        {
-          name : "Tarea 1",
-          grade : 8
-        },
-        {
-          name : "Tarea 2",
-          grade : 1
-        },
-        {
-          name : "Tarea 3",
-          grade : 9
-        },
-        {
-          name : "Tarea 4",
-          grade : 10
-        }],
-      finalGrade : 10 
-    }
-  ]
+  subjectListDto : SubjectDto[] = []
 
-  displayedColumns: string[] = [];
-  tasksColumns: string[] = [];
+  displayedColumns: string[] = ['name', 'detail'];
 
-  subject : Subject = {
-    name: '',
-    code: '',
-    instructor: '',
-    parallel: '',
-    classes: []
-  };
 
-  getTaskGrade (student : StudentContinuousEvaluation, taskName : string) {
-    const evaluation = student.continuousEvaluation.find(evaluation => evaluation.name === taskName);
 
-    if (evaluation !== undefined)
-      return evaluation.grade;
+  constructor(private studentListService: StudentListService, private continuousEvalaution: ContinuosEvaluationService ) { }
 
-    return 0;
-  }
 
-  constructor(private subjectsService: SubjectsService) { 
-    this.subjectsService.fetchSubjectContinuousEvaluation("").subscribe((response: SubjectContinuousGrades) => {   
-      this.evaluations = response.students;
-      this.subject = response.subject
-
-      this.displayedColumns.push('name');
-
-      if (this.evaluations.length > 0) {
-        for (let i = 0; i < this.evaluations[0].continuousEvaluation.length; i++) {
-          this.tasksColumns.push(this.evaluations[0].continuousEvaluation[i].name);
-          this.displayedColumns.push(this.evaluations[0].continuousEvaluation[i].name);
-        }
+  ngOnInit(): void {
+    this.studentListService.getSubjectsByProfessorIdAndSemesterId(this.professorId, this.semesterId).subscribe({
+      next: (data) => {
+        this.subjectListDto = data.data!;
+        console.log(this.subjectListDto);
+      },
+      error: (error) => {
+        console.log(error);
       }
-
-      this.displayedColumns.push('finalGrade');
-
     });
   }
+
+  downloadReport(): void {
+    console.log("downloadReport");
+    this.continuousEvalaution.getContinuosEvaluation().subscribe((response) => {
+      const data = response.data;
+      window.open(data, "_blank");
+    })
+  }
+
+
+
 }
