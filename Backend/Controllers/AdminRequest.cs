d526cd3aa41973fe363377387fd2b5ffa7b1c3e1
Sebastian Francisco Belmonte Cerveró <sebastian.belmonte01@gmail.com>
2023-10-26 11:50:59 -0400

    TDS-699 adding Admin controller

---
+++ Backend/Controllers/AdminRequest.cs
@@ -0,0 +1,51 @@
+using Backend.DTOs;
+using Backend.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
+
+namespace Backend.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class AdminRequestController : ControllerBase
+    {
+        private readonly TDSKardexRequest.TDSKardexRequestContext _dbKardexRequest = new TDSKardexRequest.TDSKardexRequestContext();
+        
+  
+
+        [HttpPut("admin/{requestId:int}/{deliverDate:datetime}")]
+        public async Task<ActionResult<ResponseDTO<TDSKardexRequest>>> UpdateKardexRequest(int requestId, [FromRoute] DateTime deliverDate)
+        {
+            try
+            {
+                Console.WriteLine("Entering the method");
+
+                var kardexRequest = await _dbKardexRequest.TDS_kardex_request.FindAsync(requestId);
+                Console.WriteLine("Kardex request found");
+
+
+                if (kardexRequest == null)
+                {
+                    return NotFound("Kardex request not found");
+                }
+
+                kardexRequest.request_state = "Aceptado";
+                kardexRequest.deliver_date = deliverDate;
+                kardexRequest.date = DateTime.Now;
+
+                _dbKardexRequest.Entry(kardexRequest).State = EntityState.Modified;
+                await _dbKardexRequest.SaveChangesAsync();
+
+                var kardexRequestUpdated = await _dbKardexRequest.TDS_kardex_request.FindAsync(requestId);
+
+                return Ok(new ResponseDTO<TDSKardexRequest>(kardexRequestUpdated, null, true));
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
