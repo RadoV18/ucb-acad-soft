6ee633bedaefc2954b1af00cf20054cac7fc90db
Sebastian Francisco Belmonte Cerveró <sebastian.belmonte01@gmail.com>
2023-10-30 15:57:12 -0400

    TDS-699 backend finished

--- Backend/Controllers/AdminRequest.cs
+++ Backend/Controllers/AdminRequest.cs
@@ -15,7 +15,7 @@ namespace Backend.Controllers
         
   
 
-        [HttpPut("admin/{requestId:int}/{deliverDate:datetime}")]
+        [HttpPut("admin/{requestId:int}/{deliverDate:datetime}/accept")]
         public async Task<ActionResult<ResponseDTO<TDSKardexRequest>>> UpdateKardexRequest(int requestId, [FromRoute] DateTime deliverDate)
         {
             try
@@ -47,5 +47,42 @@ namespace Backend.Controllers
                 return BadRequest(e.Message);
             }
         }
+        
+        //Make the same function, but for reject
+        [HttpPut("admin/{requestId:int}/reject")]
+        public async Task<ActionResult<ResponseDTO<TDSKardexRequest>>> RejectRquest(int requestId)
+        {
+            try
+            {
+                Console.WriteLine("Entering the method");
+
+                var kardexRequest = await _dbKardexRequest.TDS_kardex_request.FindAsync(requestId);
+                Console.WriteLine("Kardex request found");
+
+
+                if (kardexRequest == null)
+                {
+                    return NotFound("Kardex request not found");
+                }
+
+                kardexRequest.request_state = "Rechazado";
+                kardexRequest.deliver_date = DateTime.Now;
+                kardexRequest.date = DateTime.Now;
+
+                _dbKardexRequest.Entry(kardexRequest).State = EntityState.Modified;
+                await _dbKardexRequest.SaveChangesAsync();
+
+                var kardexRequestUpdated = await _dbKardexRequest.TDS_kardex_request.FindAsync(requestId);
+
+                return Ok(new ResponseDTO<TDSKardexRequest>(kardexRequestUpdated, null, true));
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
     }
+    
+    
+    
 }
