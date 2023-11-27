e8d752bb2f66bdcc51633bfd706d0a253cc9813a
Mauricio Flores <mauricio.flores.p@ucb.edu.bo>
2023-11-26 23:55:58 -0400

    Adding BE calls for abandon rates

---
+++ Backend/Controllers/AbandonRatesController.cs
@@ -0,0 +1,24 @@
+﻿namespace Backend.Controllers;
+
+using Backend.DTOs;
+using Backend.Models;
+using Microsoft.AspNetCore.Mvc;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AbandonRatesController : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<AbandonRatesResponseDTO>> GetAbandonRatesBySubjectId()
+    {
+        try
+        {
+            AbandonRatesResponseDTO abandonRates = new(new List<GraphValues>(), new List<GraphValues>(), new List<GraphValues>());
+            return Ok(abandonRates);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+}
---
+++ Backend/DTOs/AbandonRatesResponseDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Backend.DTOs;
+
+using Backend.Models;
+
+public record AbandonRatesResponseDTO
+(
+    List<GraphValues> ByGrades,
+    List<GraphValues> ByMonths,
+    List<GraphValues> BySubjects
+);
+
