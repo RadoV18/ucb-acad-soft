7e6fb331ddbb241bb883c0b91bde7540bddaa8b7
Sebastian Francisco Belmonte Cerveró <sebastian.belmonte01@gmail.com>
2023-11-28 18:15:45 -0400

    Adding dashboard on kardex

--- frontend/src/app/pages/student-kardex/student-kardex.component.ts
+++ frontend/src/app/pages/student-kardex/student-kardex.component.ts
@@ -4,6 +4,27 @@ import {MatPaginator} from "@angular/material/paginator";
 import {MatSort} from "@angular/material/sort";
 import {KardexService} from "../../services/kardex-sevice/kardex.service";
 import {SemesterResume} from "../../models/SemesterResume";
+import {
+  ChartComponent,
+  ApexAxisChartSeries,
+  ApexChart,
+  ApexXAxis,
+  ApexDataLabels,
+  ApexTitleSubtitle,
+  ApexStroke,
+  ApexGrid
+} from "ng-apexcharts";
+
+export type ChartOptions = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  xaxis: ApexXAxis;
+  dataLabels: ApexDataLabels;
+  grid: ApexGrid;
+  stroke: ApexStroke;
+  title: ApexTitleSubtitle;
+};
+
 
 @Component({
   selector: 'app-student-kardex',
@@ -11,6 +32,8 @@ import {SemesterResume} from "../../models/SemesterResume";
   styleUrls: ['./student-kardex.component.sass']
 })
 export class StudentKardexComponent implements OnInit {
+  @ViewChild("chart") chart!: ChartComponent;
+  public chartOptions: Partial<ChartOptions>;
 
 
   displayedColumns: string[] = [
@@ -35,6 +58,56 @@ export class StudentKardexComponent implements OnInit {
    kardex: SemesterResume | any;
 
   constructor(private kardexService: KardexService) {
+
+    this.chartOptions = {
+      series: [
+        {
+          name: "Desktops",
+          data: [10, 41, 35, 51, 49, 62, 69, 91, 148],
+        }
+      ],
+      chart: {
+        height: 350,
+        type: "line",
+        zoom: {
+          enabled: false
+        }
+      },
+      dataLabels: {
+        enabled: false
+      },
+      stroke: {
+        curve: "straight"
+      },
+      title: {
+        text: "Product Trends by Month",
+        align: "left"
+      },
+      grid: {
+        row: {
+          colors: ["#ffffff"],
+          opacity: 0.5
+        }
+      },
+      xaxis: {
+        categories: [
+          "Jan",
+          "Feb",
+          "Mar",
+          "Apr",
+          "May",
+          "Jun",
+          "Jul",
+          "Aug",
+          "Sep"
+        ],
+        labels: {
+          style: {
+            colors: "#ffffff"
+          }
+        }
+      }
+    };
   }
 
   ngOnInit(): void {
