12d778e792a419a6a66d187563413accf3d88c21
Radomir Vladislavić <vladislavic.rl@gmail.com>
2023-11-29 19:50:13 -0400

    TDS-17 add student averages pdf report

--- Backend/Controllers/StudentAveragesController.cs
+++ Backend/Controllers/StudentAveragesController.cs
@@ -19,6 +19,8 @@ public class StudentAveragesController : ControllerBase
         "Arquitectura",
         "Ciencias Politicas",
     };
+    private readonly Services.PdfTurtleService _pdfTurtleService = new();
+    private readonly Services.MinioService _minioService = new();
 
     [HttpGet]
     public async Task<ActionResult<List<StudentAverageDTO>>> GetStudentsAverages()
@@ -47,5 +49,33 @@ public class StudentAveragesController : ControllerBase
         }
 
         return list;
+    }
+
+    [HttpGet("pdf")]
+    public async Task<ActionResult<ResponseDTO<string>>> GetStudentsAveragesPDF()
+    {
+        try
+        {
+            var header = System.IO.File.ReadAllText("Utils/PdfTemplates/StudentAverages/header.html");
+            var footer = System.IO.File.ReadAllText("Utils/PdfTemplates/StudentAverages/footer.html");
+            var body = System.IO.File.ReadAllText("Utils/PdfTemplates/StudentAverages/index.html");
+
+            var model = new
+            {
+                data = GetStudentsAveragesList()
+            };
+
+            byte[] pdf = await _pdfTurtleService.getPdf(footer, header, body, model);
+
+            // upload pdf to minio
+            Guid guid = Guid.NewGuid();
+            string fileName = $"{guid}.pdf";
+            var newFileDto = await _minioService.UploadFile("pdf", fileName, pdf, "application/pdf");
+            return Ok(new ResponseDTO<string>(newFileDto.DownloadLink, null, true));
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
     }
 }
