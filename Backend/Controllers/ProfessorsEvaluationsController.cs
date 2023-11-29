ee36d6afd884afc95aae380fc21d297d3390cf29
Radomir Vladislavić <vladislavic.rl@gmail.com>
2023-11-29 19:37:05 -0400

    TDS-17 add professor evaluation pdf report

--- Backend/Controllers/ProfessorsEvaluationsController.cs
+++ Backend/Controllers/ProfessorsEvaluationsController.cs
@@ -12,6 +12,8 @@ public class ProfessorsEvaluationsController : ControllerBase
     {
         "Ingenieria", "Comunicacion", "Arquitectura", "Derecho", "Economia", "Ciencias Sociales", "Ciencias de la Salud", "Ciencias Politicas", "Humanidades", "Ciencias Basicas"
     };
+    private readonly Services.PdfTurtleService _pdfTurtleService = new();
+    private readonly Services.MinioService _minioService = new();
 
     [HttpGet]
     public async Task<ActionResult<List<EvaluatedProfessor>>> GetProfessorsEvaluations()
@@ -19,6 +21,34 @@ public class ProfessorsEvaluationsController : ControllerBase
         try
         {
             return Ok(GetProfessorsEvaluationsList());
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+
+    [HttpGet("pdf")]
+    public async Task<ActionResult<ResponseDTO<string>>> GetProfessorsEvaluationsPDF()
+    {
+        try
+        {
+            var header = System.IO.File.ReadAllText("Utils/PdfTemplates/ProfessorEvaluations/header.html");
+            var footer = System.IO.File.ReadAllText("Utils/PdfTemplates/ProfessorEvaluations/footer.html");
+            var body = System.IO.File.ReadAllText("Utils/PdfTemplates/ProfessorEvaluations/index.html");
+
+            var model = new
+            {
+                data = GetProfessorsEvaluationsList()
+            };
+
+            byte[] pdf = await _pdfTurtleService.getPdf(footer, header, body, model);
+
+            // upload pdf to minio
+            Guid guid = Guid.NewGuid();
+            string fileName = $"{guid}.pdf";
+            var newFileDto = await _minioService.UploadFile("pdf", fileName, pdf, "application/pdf");
+            return Ok(new ResponseDTO<string>(newFileDto.DownloadLink, null, true));
         }
         catch (Exception e)
         {
